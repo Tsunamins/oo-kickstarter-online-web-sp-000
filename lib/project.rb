a5389cd01050e327004183bd3a14ea25b70e56f9
Alison Reilly <reillyamr@gmail.com>
2019-01-04 22:19:05 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,5 @@
 class Project 
-  attr_accessor :backers, :title, :project 
+  attr_accessor :backers, :title
   
   def initialize(title) 
     @title = title
@@ -15,6 +15,9 @@ class Project
     # self.title = backer 
     # title.back_project(self)
   
+    @backed_projects << project.title
   
-  end 
+  end
+  
+  
 end 
